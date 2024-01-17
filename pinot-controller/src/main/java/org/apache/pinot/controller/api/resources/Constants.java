5fa4132e6a8d6bdedbad00fdb2b0129cffa06e70
Shounak kulkarni <shounakmk219@gmail.com>
2024-01-17 16:38:00 +0530

    Add request filter to controller APIs
    Translate database name to Id and table name to FQN

--- pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/Constants.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/Constants.java
@@ -47,6 +47,7 @@ public class Constants {
   public static final String TASK_TAG = "Task";
   public static final String LEAD_CONTROLLER_TAG = "Leader";
   public static final String TABLE_NAME = "tableName";
+  public static final String DATABASE = "database";
   public static final String ZOOKEEPER = "Zookeeper";
   public static final String APP_CONFIGS = "AppConfigs";
   public static final String PERIODIC_TASK_TAG = "PeriodicTask";
